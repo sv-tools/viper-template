623ca122fdcf1868feed1f755dbdd434a34d5105
Sergey Vilgelm <sergey@vilgelm.com>
2021-11-06 19:24:28 -0700

    Fix govet issue

--- errors.go
+++ errors.go
@@ -6,12 +6,12 @@ import (
 )
 
 func newError(key string, err error) error {
-	return fmt.Errorf("non parsable template for the key '%s': %w", key, err)
+	return fmt.Errorf("non-parsable template for the key '%s': %w", key, err)
 }
 
 var (
 	// ErrCircularDependency is an error returned if the circular dependency detected
 	ErrCircularDependency = errors.New("circular dependency")
 	// ErrNonStringValue is an error returned if the requested value is not a string
-	ErrNonStringValue = errors.New("non string value")
+	ErrNonStringValue = errors.New("non-string value")
 )
